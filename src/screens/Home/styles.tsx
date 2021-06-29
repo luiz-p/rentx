8ddcc941dad5669b590026b3da5ae7e38c10c0a6
Luiz Pires <lpiresjunior@gmail.com>
2021-06-29 17:44:26 -0300

    feat: add global theme

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -5,6 +5,11 @@ export default {
     flex: 1;
     justify-content: center;
     align-items: center;
+
+    background-color: ${({ theme }) => theme.colors.background_primary};
   `,
-  Title: styled.Text``,
+  Title: styled.Text`
+    font-size: 30px;
+    font-family: ${({ theme }) => theme.fonts.secondary_600};
+  `,
 };

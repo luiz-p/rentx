fc8c9aabedc5c33a1f38e8ff9b1a6015c50a497a
Luiz Pires <lpiresjunior@gmail.com>
2021-07-01 23:48:38 -0300

    style(Accessory): card width

--- src/components/Accessory/styles.ts
+++ src/components/Accessory/styles.ts
@@ -1,22 +1,26 @@
+import { Dimensions } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
+const width = Dimensions.get('window').width;
+
 export default {
-  Container: styled.SafeAreaView`
-    width: 109px;
-    height: 92px;
+  Container: styled.View`
+    width: ${width / 3 - 64 / 3}px;
+    min-height: 92px;
 
     justify-content: center;
     align-items: center;
 
     background-color: ${({ theme }) => theme.colors.background_primary};
 
-    padding: 16px;
+    padding: 16px 8px;
     margin-bottom: 8px;
   `,
 
   Name: styled.Text`
     font-family: ${({ theme }) => theme.fonts.primary_500};
     color: ${({ theme }) => theme.colors.text};
-    font-size: 13px;
+    font-size: ${RFValue(13)}px;
   `,
 };
